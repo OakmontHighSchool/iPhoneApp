db86a518b13fd1151428f457644414219d6e5642
Jon Janzen <jjjonjanzen@gmail.com>
2014-08-11 21:50:04 -0700

    Cleanup of headlines view controller

--- OHSApp/OHSNewsViewController.m
+++ OHSApp/OHSNewsViewController.m
@@ -19,7 +19,7 @@
 
 - (IBAction)unwindToArticleOverview:(UIStoryboardSegue *)segue
 {
-    
+    //Necessary for some reason
 }
 
 - (IBAction)reloadArticles:(id)sender {
@@ -28,7 +28,6 @@
 
 - (void)downloadNewsArticles
 {
-    // 1
     NSURL *newsUrl = [NSURL URLWithString:@"http://ohs.rjuhsd.us/site/default.aspx?PageID=1"];
     NSData *newsHtmlData = [NSData dataWithContentsOfURL:newsUrl];
     
@@ -48,11 +47,8 @@
     NSMutableArray *newArticles = [[NSMutableArray alloc] initWithCapacity:10];
     for(int i=0;i<[titleNodes count];i++) {
         OHSNewsArticle *article = [[OHSNewsArticle alloc] init];
-        
         article.title = [[titleNodes objectAtIndex:i] content];
-        
         article.link = [[linkNodes objectAtIndex:i] text];
-        
         [newArticles addObject:article];
     }
     
@@ -60,24 +56,9 @@
     [self.tableView reloadData];
 }
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     [self downloadNewsArticles];
 }
 
@@ -97,12 +78,6 @@
         url = [NSString stringWithFormat:@"%@%@", @"http://ohs.rjuhsd.us", url];
         controller.url = url;
     }
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Table view data source
@@ -128,54 +103,5 @@
     
     return cell;
 }
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
