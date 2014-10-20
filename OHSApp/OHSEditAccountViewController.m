ce42f6ffdb126da61a2dfbec290d7630121f12ef
Jon Janzen <jjjonjanzen@gmail.com>
2014-10-19 17:14:35 -0700

    Fixed a bug where creating a new account, then editing any other account caused that account to be treated as a new account (saved as new row)

--- OHSApp/OHSEditAccountViewController.m
+++ OHSApp/OHSEditAccountViewController.m
@@ -18,6 +18,8 @@ BOOL newAccount = NO;
     if(self.account == nil) {
         newAccount = YES;
         self.account = [[OHSAccount alloc] init];
+    } else {
+        newAccount = NO;
     }
     
     [self.nameField setText:self.account.name];
@@ -85,10 +87,7 @@ BOOL newAccount = NO;
 }
 
 -(BOOL) NSStringIsValidEmail:(NSString *)checkString {
-    BOOL stricterFilter = NO; // Discussion http://blog.logichigh.com/2010/09/02/validating-an-e-mail-address/
-    NSString *stricterFilterString = @"[A-Z0-9a-z\\._%+-]+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}";
-    NSString *laxString = @".+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2}[A-Za-z]*";
-    NSString *emailRegex = stricterFilter ? stricterFilterString : laxString;
+    NSString *emailRegex = @".+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2}[A-Za-z]*";
     NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
     return [emailTest evaluateWithObject:checkString];
 }
